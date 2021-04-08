1d756611957a63c246807606b80be4cae7e37c79
Andrei Iulian Luca <luca.andrei@info.uaic.ro>
2021-04-09 00:52:12 +0300

    added selection subject page + css

--- components/header.php
+++ components/header.php
@@ -3,6 +3,7 @@ require_once('./includes/functions.php');
 ?>
 <head>
     <title>Class Manager</title>
+    <link rel="stylesheet" href="<?php echo ''.URL.''?>assets/selection.css">
     <link rel="stylesheet" href="<?php echo ''.URL.''?>assets/about.css">
     <link rel="stylesheet" href="<?php echo ''.URL.''?>assets/login-register.css">
     <link rel="stylesheet" href="<?php echo ''.URL.''?>assets/style.css">
