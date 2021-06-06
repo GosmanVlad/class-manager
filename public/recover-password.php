786da0dddac695c43757e18f02a56488da7c6465
Andrei Iulian Luca <luca.andrei@info.uaic.ro>
2021-06-06 15:59:38 +0300

    added small responsive changes for log-in, register pages

--- public/recover-password.php
+++ public/recover-password.php
@@ -12,8 +12,14 @@ if(isLogged())
         <h2>Recover your password</h2>
         <img src="<?php echo '' . URL . '' ?>assets/images/recover.svg" width="50" height="50" alt="Recover Logo">
         <div class="margin-20">
-            <label for="uname"><b>E-mail</b></label>
+            <label class= "label-recover" for="uname"><b>E-mail</b></label>
             <input class="input-login-register button" type="text" placeholder="Enter E-mail" id="uname" required>
+            <label class= "label-recover" for="uname"><b>Old Password</b></label>
+            <input class="input-login-register button" type="text" placeholder="Enter your old Password" id="uname" required>
+            <label class= "label-recover" for="uname"><b>New Password</b></label>
+            <input class="input-login-register button" type="text" placeholder="Enter your new Password" id="uname" required>
+            <label class= "label-recover" for="uname"><b>Confirm New Password</b></label>
+            <input class="input-login-register button" type="text" placeholder="Re-Enter your new Password" id="uname" required>
         </div>
 
         <button class="button-style btn-pencil btn-cyan btn-full btn-medium cursor" type="submit">Recover</button>
