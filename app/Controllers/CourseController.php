25bbc8759e12d0ca5d7b43ac484d16b37c1077aa
Andrei Iulian Luca <luca.andrei@info.uaic.ro>
2021-06-07 19:53:19 +0300

    solved bug on animated cards (same group repeated)

--- app/Controllers/CourseController.php
+++ app/Controllers/CourseController.php
@@ -1,34 +1,39 @@
 <?php
 require_once "Database.php";
-class Course {
+class Course
+{
 
-    public function getCourseByID($courseID) {
+    public function getCourseByID($courseID)
+    {
         $result = Database::dbQuery("SELECT * FROM courses WHERE id = $courseID", (new Database()));
         $result->execute();
 
-        if($result->rowCount())
+        if ($result->rowCount())
             return $result->fetch();
         http_response_code(404);
         return 0;
     }
 
-    public function getCourseCredits($courseID) {
+    public function getCourseCredits($courseID)
+    {
         $result = Database::dbQuery("SELECT * FROM courses WHERE id = $courseID", (new Database()));
         $result->execute();
 
-        if($result->rowCount())
+        if ($result->rowCount())
             return $result->fetch()['credits'];
         http_response_code(404);
         return 0;
     }
 
-    public function getAllStudents($course) {
+    public function getAllStudents($course)
+    {
         $result = Database::dbQuery("SELECT * FROM allocations WHERE course_id = $course", (new Database()));
         $result->execute();
         return $result->rowCount();
     }
 
-    public function getAllAssignedGrades($teacher, $course) {
+    public function getAllAssignedGrades($teacher, $course)
+    {
         $result = Database::dbQuery("SELECT * FROM holders h 
                                     JOIN courses c ON h.course_id = c.id 
                                     JOIN grades g ON g.course_id = c.id 
@@ -37,25 +42,30 @@ class Course {
         return $result->rowCount();
     }
 
-    public function getTeachClasses($teacher, $course) {
+    public function getTeachClasses($teacher, $course)
+    {
         $result = Database::dbQuery("SELECT * FROM allocations a 
                                     JOIN students s ON a.student_id = s.id 
                                     WHERE a.teacher_id = $teacher AND a.course_id = $course", (new Database()));
         $result->execute();
 
+        $groupYearArray = array();
         $index = 0;
         $sendResult = '';
 
-        if($result->rowCount()) {
+        if ($result->rowCount()) {
             $fetch = $result->fetchAll();
-            foreach($fetch as $row) {
-                if($index == 0) {
-                    $sendResult = $row['year'] . $row['group_letter'];
+            foreach ($fetch as $row) {
+                $combination = $row['year'] . $row['group_letter'];
+                if (!in_array($combination, $groupYearArray)) {
+                    if ($index == 0) {
+                        $sendResult = $row['year'] . $row['group_letter'];
+                    } else {
+                        $sendResult = $sendResult . ', ' . $row['year'] . $row['group_letter'];
+                    }
+                    $index++;
+                    array_push($groupYearArray, $combination);
                 }
-                else {
-                    $sendResult = $sendResult . ', ' . $row['year'] . $row['group_letter'];
-                }
-                $index++;
             }
             return $sendResult;
         }
